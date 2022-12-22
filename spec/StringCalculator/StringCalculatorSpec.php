7d122fdf5749842cc173310431f5adcc4f2c4b82
pratik <Pratik.devstree@gmail.com>
2022-12-22 16:18:43 +0530

    perform additional substraction

--- spec/StringCalculator/StringCalculatorSpec.php
+++ spec/StringCalculator/StringCalculatorSpec.php
@@ -2,7 +2,6 @@
 
 namespace spec\StringCalculator;
 
-use InvalidArgumentException;
 use PhpSpec\ObjectBehavior;
 use StringCalculator\StringCalculator;
 
@@ -15,12 +14,18 @@ class StringCalculatorSpec extends ObjectBehavior
 
     public function it_can_perform_addition()
     {
-        $this->calculate("1+19")->shouldReturn(20);
+        $this->calculate('1 + 19')->shouldReturn(20);
     }
 
     public function it_throws_an_exception_when_input_is_invalid()
     {
-        $this->shouldThrow(InvalidArgumentException::class)
-            ->during('calculate', ["1+"]);
+        $this->shouldThrow(\InvalidArgumentException::class)
+            ->during('calculate', ['1 +'])
+        ;
+    }
+
+    public function it_can_perform_additional_substraction()
+    {
+        $this->calculate('1 + 19 - 2')->shouldReturn(18);
     }
 }
--- src/StringCalculator/StringCalculator.php
+++ src/StringCalculator/StringCalculator.php
@@ -2,31 +2,38 @@
 
 namespace StringCalculator;
 
-use InvalidArgumentException;
-
 class StringCalculator
 {
     public function calculate(string $argument): int
     {
-        $matches =   $this->parseString($argument);
-
+        $matches = explode(' ', $argument);
+        var_dump($matches);
         if (!$this->validateString($matches)) {
-            throw new InvalidArgumentException;
+            throw new \InvalidArgumentException();
+        }
+        while (count($matches) > 2) {
+            switch ($matches[1]) {
+                case '+':
+                    $result = $matches[0] + $matches[2];
+
+                    break;
+
+                case '-':
+                    $result = $matches[0] - $matches[2];
+
+                    break;
+            }
+
+            unset($matches[0],$matches[1],$matches[2]);
+            array_unshift($matches, $result);
         }
 
-        switch($matches[1]) {
-            case "+":
-                $result = $matches[0] + $matches[2];
-                break;
-                // etc
-        }
-
-        return $result;
+        return $matches[0];
     }
 
     private function parseString(string $argument): array
     {
-        $pattern = "/(\d+)(\+)(\d+)/";
+        $pattern = '/(\\d+)(\\+)(\\d+)/';
         preg_match($pattern, $argument, $matches);
 
         array_shift($matches);

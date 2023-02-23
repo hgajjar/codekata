a6021802d5d67e9786a5c37c8c7bab48a4d87bc8
Hardik Gajjar <hgajjar@inviqa.com>
2023-02-23 18:33:07 +0530

    Strike and Spare

--- spec/BowlingGame/GameSpec.php
+++ spec/BowlingGame/GameSpec.php
@@ -37,4 +37,56 @@ class GameSpec extends ObjectBehavior
 
         $this->getScore()->shouldReturn(20);
     }
+
+    public function it_can_calculate_score_with_spare()
+    {
+        $this->play(5);
+        $this->play(5);
+        $this->play(4);
+        $this->play(6);
+        $this->play(3);
+        $this->play(7);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(2);
+        $this->play(8);
+        $this->play(1);
+        $this->play(1);
+
+        $this->getScore()->shouldReturn(61);
+    }
+
+    public function it_can_calculate_spare_and_strike()
+    {
+        $this->play(10);
+
+        $this->play(0);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(1);
+        $this->play(10);
+
+        $this->play(1);
+        $this->play(1);
+
+        $this->getScore()->shouldReturn(38);
+    }
 }
--- src/BowlingGame/Game.php
+++ src/BowlingGame/Game.php
@@ -10,7 +10,10 @@ class Game
     public function play(int $score): void
     {
         $lastFrame = end($this->frames);
-        if (!$lastFrame || (null !== $lastFrame->getFirstShot() && null !== $lastFrame->getSecondShot())) {
+
+        if (!$lastFrame // initial
+            || (null !== $lastFrame->getFirstShot() && null !== $lastFrame->getSecondShot()) // both shots have values
+            || 10 === $lastFrame->getFirstShot()) { // strike
             $lastFrame = new Frame();
             $this->frames[] = $lastFrame;
         }
@@ -24,6 +27,21 @@ class Game
 
     public function getScore(): int
     {
-        return array_reduce($this->frames, fn (int $carry, Frame $item): int => $carry + $item->getFirstShot() + $item->getSecondShot(), 0);
+        $totalScore = 0;
+
+        foreach ($this->frames as $index => $frame) {
+            $totalScore += $frame->getFirstShot();
+            $totalScore += $frame->getSecondShot();
+
+            if (10 === $frame->getFirstShot()) {
+                // strike
+                $totalScore += ($this->frames[$index + 1]->getFirstShot() + $this->frames[$index + 1]->getSecondShot());
+            } elseif (($frame->getFirstShot() + $frame->getSecondShot()) == 10) {
+                // spare
+                $totalScore += $this->frames[$index + 1]->getFirstShot();
+            }
+        }
+
+        return $totalScore;
     }
 }
